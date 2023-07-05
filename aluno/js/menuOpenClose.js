6007da929d55bf86dfd94db5e2a28a633fa0960d
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-07-05 12:12:07 -0300

    responsividade nos professores

--- aluno/js/menuOpenClose.js
+++ aluno/js/menuOpenClose.js
@@ -1,10 +1,12 @@
 function menuShow(){
-    if(document.getElementById("menu").style.display == "block" && window.innerWidth < 768){
+    if(document.getElementById("menu").style.display == "block")
+    {
         document.getElementById("menu").style.display = "none";
-    }else if(window.innerWidth < 768){
+    }else if(window.innerWidth <= 1024){
         document.getElementById("menu").style.display = "block";
         document.getElementById("menu").style.marginTop = "5rem";
-        document.getElementById("menu").style.width = "calc(40% + 100px)";
+        document.getElementById("fotoPerfil").style.marginBottom = "calc(14vh - 3rem)";
+        document.getElementById("menu").style.width = "calc(35% + 100px)";
         document.getElementById("menu").style.height = "calc(100% - 5rem)";
     }
 }
