18ed7c57e7d24942ae89b15cfee303d1998d60c6
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-07-05 14:08:35 -0300

    terminando design

--- aluno/js/menuOpenClose.js
+++ aluno/js/menuOpenClose.js
@@ -4,7 +4,6 @@ function menuShow(){
         document.getElementById("menu").style.display = "none";
     }else if(window.innerWidth <= 768){
         document.getElementById("menu").style.display = "block";
-        document.getElementById("menu").style.marginTop = "5rem";
         document.getElementById("fotoPerfil").style.marginBottom = "calc(14vh - 3rem)";
         document.getElementById("menu").style.width = "calc(40% + 150px)";
         document.getElementById("menu").style.height = "calc(100% - 5rem)";
