7822c6f3f49c214be1fc3f6bc57c05d952cbd08b
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-07-14 16:24:07 -0300

    style/css notificações terminada

--- aluno/js/notifyOpenClose.js
+++ aluno/js/notifyOpenClose.js
@@ -1,11 +1,22 @@
 const openNotify = document.querySelector('#openNotify');
+const openNotifyMobile = document.querySelector('#openNotifyMobile');
 const closeNotify = document.querySelector('#closeNotify');
 const notify = document.querySelector('#notificações');	
 
 openNotify.addEventListener('click', function() {
-    notify.show();
+    notify.showModal();
+});
+
+openNotifyMobile.addEventListener('click', function() {
+    notify.showModal();
 });
 
 closeNotify.addEventListener('click', function() {
     notify.close();
 });
+
+notify.addEventListener('click', event => {
+    if (event.target === event.currentTarget) {
+        event.currentTarget.close()
+    }
+})
