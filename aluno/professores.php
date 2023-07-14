31140917c23f32d2152647a25ed0d0ede256084c
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-07-14 12:03:31 -0300

    arrumando src e iniciando os modals

--- aluno/professores.php
+++ aluno/professores.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Suas Informações</title>
-    <link rel="stylesheet" type="text/css" href="style/estiloPadrao.css">
-    <link rel="stylesheet" type="text/css" href="style/estiloMenuAluno.css">
-    <link rel="stylesheet" type="text/css" href="style/estiloProfessores.css">
+    <link rel="stylesheet" type="text/css" href="styles/estiloPadrão.css">
+    <link rel="stylesheet" type="text/css" href="styles/estiloProfessores.css">
 </head>
 
 <body id="bodyProfessores">
@@ -17,7 +16,7 @@
         <section>
             <h1 id="title">Professores</h1>
             <div id="container">
-                <iframe src="./includes/professoresDisponiveis.php" frameborder="0"></iframe>
+                <iframe src="./includes/professoresDisponíveis.php" frameborder="0"></iframe>
             </div>
         </section>
     </main>
