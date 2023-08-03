25ee1b636779766d9a7821846fe86bcd45bc2830
unknown <matheusq057@gmail.com>
2023-08-03 11:08:14 -0300

    add todo mundo

---
+++ professor/js/menuOpenClose.js
@@ -0,0 +1,21 @@
+function menuShow(){
+    if(document.getElementById("menu").style.display == "block")
+    {
+        document.getElementById("menu").style.display = "none";
+    }else if(window.innerWidth <= 768){
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("fotoPerfil").style.marginBottom = "calc(14vh - 3rem)";
+        document.getElementById("menu").style.width = "calc(40% + 150px)";
+        document.getElementById("menu").style.height = "calc(100% - 5rem)";
+
+    }
+}
+
+function retornaTituloResponsivo() {
+    var title = document.getElementById("title").textContent;
+    return title;
+  }
+  
+  var paragrafo = document.getElementById("titleResponsive");
+  paragrafo.textContent = retornaTituloResponsivo();
+  
