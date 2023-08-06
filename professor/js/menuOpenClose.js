eaba55c8f25c744f2432ed5008be912377453a5c
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-08-06 12:29:28 -0300

    arrumando versão mobile do menu

--- professor/js/menuOpenClose.js
+++ professor/js/menuOpenClose.js
@@ -1,13 +1,13 @@
 function menuShow(){
-    if(document.getElementById("menu").style.display == "block")
+    if(document.getElementById("menu").style.display == "flex")
     {
         document.getElementById("menu").style.display = "none";
     }else if(window.innerWidth <= 768){
-        document.getElementById("menu").style.display = "block";
+        document.getElementById("menu").style.display = "flex";
         document.getElementById("fotoPerfil").style.marginBottom = "calc(14vh - 3rem)";
         document.getElementById("menu").style.width = "calc(40% + 150px)";
         document.getElementById("menu").style.height = "calc(100% - 5rem)";
-
+        document.getElementById("menu").style.fontSize = "1.3rem";
     }
 }
 
