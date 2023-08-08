f6bbb24ad3ad8c5a6476baa86e991aae29ad600c
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-08-07 22:08:50 -0300

    Adicionar Aluno Desktop

--- professor/alunos.php
+++ professor/alunos.php
@@ -58,11 +58,27 @@
                             <a id="saibaMais" href="google.com">Saiba Mais</a>
                         </li>
                     </ul>
-                    <button id="adicionarAluno">Adicionar Aluno</button>
+                    <button id="abrirAdicionarAluno">Adicionar Aluno</button>
                 </div>
         </section>
     </main>
 </body>
+<dialog id="addAluno">
+    <div id="addAluno-content">
+        <form action="" method="post" id="formAddAluno">
+            <h1>Adicionar Aluno</h1>
+            <div id="idAluno">
+            <label for="inputIdAluno"><b>ID do aluno:</b></label>
+            <input type="text" name="idAluno" id="inputIdAluno" placeholder="ID do Aluno">
+            </div>
+            <div id="submitAddAluno">
+                <input type="submit" id="adicionarAluno" value="Adicionar Aluno">
+                <button id="closeAddAluno">Cancelar</button>
+            </div>
+        </form>
+    </div>
+</dialog>
+<script src="./js/addAlunoOpenClose.js"></script>
 <script src="./js/menuOpenClose.js"></script>
 <?php include_once "includes/modalNotificar.php"; ?>
 
