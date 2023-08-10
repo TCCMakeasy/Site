18329cd09040aacd510c478049a0e567d2995f7d
unknown <tccmakeasy@gmail.com>
2023-08-10 14:49:38 -0300

    QTS

--- adm/edit_infos.php
+++ adm/edit_infos.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,22 +8,39 @@
     <link rel="stylesheet" type="text/css" href="./styles/estiloPadrão.css">
     <link rel="stylesheet" type="text/css" href="./styles/estiloEditInfos.css">
 </head>
+
 <body>
     <?php include_once "./includes/menuAdm.php"; ?>
     <main>
         <section>
-             <h1 id="title">Suas Informações</h1>
-             <form>
-             <div id="editFoto">
-                <img src="./images/nathan.png" id="fotoPerfil" accept="./images/*">
-                <label for="inputFoto" id="labelInputFoto"><img id="imgLabelInput" src="./images/edit.png" alt="Botão para editar Foto de Perfil"></label>
-                <input type="file" id="inputFoto">
-             </div>
-             <div id="editInfos">
-                <p>Nome:</p>
-             </div>
-             </form>
+            <h1 id="title">Suas Informações</h1>
+            <form id="formInfos">
+                <div id="editFoto">
+                    <img src="./images/nathan.png" id="fotoPerfil" accept="./images/*">
+                    <label for="inputFoto" id="labelInputFoto"><img id="imgLabelInput" src="./images/edit.png" alt="Botão para editar Foto de Perfil"></label>
+                    <input type="file" id="inputFoto">
+                </div>
+                <div id="editInfos">
+                    <div id="divNome" class="divInputText">
+                        <p id="nomeTitulo" class="tituloForm">Nome:</p>
+                        <input id="nomeInput" class="inputText" type="text">
+                    </div>
+                    <div id="divId" class="divInputText">
+                        <p id="idTitulo" class="tituloForm">ID:</p>
+                        <input id="idInput" class="inputLock" type="text" value="" disabled>
+                    </div>
+                    <div id="divEmail" class="divInputText">
+                        <p id="emailTitulo" class="tituloForm">Email:</p>
+                        <input id="emailInput" class="inputText" type="text">
+                    </div>
+                    <div id="divSenha" class="divInputText">
+                        <p id="senhaTitulo" class="tituloForm">Senha:</p>
+                        <input id="senhaInput" class="inputText" type="password">
+                    </div>
+                </div>
+            </form>
         </section>
     </main>
 </body>
+
 </html>
