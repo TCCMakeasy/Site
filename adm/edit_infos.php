3a12d4cb49513985357e0ec051b88aa672353ef9
unknown <tccmakeasy@gmail.com>
2023-08-10 15:12:57 -0300

    QTS2

--- adm/edit_infos.php
+++ adm/edit_infos.php
@@ -12,18 +12,18 @@
 <body>
     <?php include_once "./includes/menuAdm.php"; ?>
     <main>
-        <section>
+        <section id="tela">
             <h1 id="title">Suas Informações</h1>
             <form id="formInfos">
-                <div id="editFoto">
+                <section id="editFoto">
                     <img src="./images/nathan.png" id="fotoPerfil" accept="./images/*">
                     <label for="inputFoto" id="labelInputFoto"><img id="imgLabelInput" src="./images/edit.png" alt="Botão para editar Foto de Perfil"></label>
                     <input type="file" id="inputFoto">
-                </div>
-                <div id="editInfos">
+                </section>
+                <section id="editInfos">
                     <div id="divNome" class="divInputText">
                         <p id="nomeTitulo" class="tituloForm">Nome:</p>
-                        <input id="nomeInput" class="inputText" type="text">
+                        <input id="nomeInput" class="inputLock" type="text" disabled>
                     </div>
                     <div id="divId" class="divInputText">
                         <p id="idTitulo" class="tituloForm">ID:</p>
@@ -31,13 +31,17 @@
                     </div>
                     <div id="divEmail" class="divInputText">
                         <p id="emailTitulo" class="tituloForm">Email:</p>
-                        <input id="emailInput" class="inputText" type="text">
+                        <p class="inputP"><input id="emailInput" class="inputText" type="text"><img src="./images/edit.png"></p>
                     </div>
                     <div id="divSenha" class="divInputText">
                         <p id="senhaTitulo" class="tituloForm">Senha:</p>
-                        <input id="senhaInput" class="inputText" type="password">
+                        <p class="inputP"><input id="senhaInput" class="inputText" type="password"><img src="./images/edit.png"></p>
                     </div>
-                </div>
+                    <div id="divDataNasc" class="divInputText">
+                        <p id="dataNascTitulo" class="tituloForm">Data de Nascimento:</p>
+                        <input id="dataNascInput" class="inputLock" type="text" value="" disabled>
+                    </div>
+                </section>
             </form>
         </section>
     </main>
