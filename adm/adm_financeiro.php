dd6b054ba42fe0a0d61120f5eb37276856a2cd3b
unknown <tccmakeasy@gmail.com>
2023-09-06 13:44:50 -0300

    tcc

--- adm/adm_financeiro.php
+++ adm/adm_financeiro.php
@@ -24,8 +24,15 @@ if (!isset($_SESSION['id']) || $_SESSION['tipo'] != 3) {
         <h1 id="title">Financeiro</h1>
         <div id="container">
             <h1 id="title">Lucro Mensal</h1>
+            <div id="botoes">
+                <button id="abrirAddGanho">Adicionar ganho</button>
+                <button id="abrirAddGasto">Adicionar gasto</button>
+                <button id="abrirValores">Editar/Excluir valor</button>
+            </div>
         </div>
     </main>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<?php include_once "includes/modalNotificar.php"; ?>
 
 </html>
