f893c823e4b54a8d781ef219edeb82c52b8d51a8
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-09-23 10:40:43 -0300

    sistema de pesquisa professor:

--- aluno/includes/pesquisa.php
+++ aluno/includes/pesquisa.php
@@ -2,9 +2,16 @@
     require_once "../../conexao.php";
     $hndl = fopen("php://input", "r");
     $texto = fread($hndl, 1024);
-    $procura = "SELECT * from professor where nome_professor LIKE '%".$texto."%'";
+    $procura = "SELECT id_professor, nome_professor, foto_professor FROM professor WHERE nome_professor LIKE '%" . $texto . "%'";
     $mysql = mysqli_query($sql, $procura);
-    $row_pesquisa = mysqli_fetch_assoc($mysql);
-    echo json_encode($row_pesquisa);
+    $resultados = array(); // Inicializa um array para armazenar os resultados
 
-?>
+    while ($row_pesquisa = mysqli_fetch_assoc($mysql)) {
+        $resultados[] = $row_pesquisa; // Adiciona cada resultado encontrado ao array
+    }
+    if (count($resultados) == 0) {
+        echo json_encode(array("erro" => "Nenhum resultado encontrado"));
+    }else{
+    echo json_encode($resultados); // Retorna todos os resultados como JSON
+}
+    ?>
--- aluno/js/professoresSearch.js
+++ aluno/js/professoresSearch.js
@@ -1,17 +1,50 @@
 const text = document.getElementById("inputPesquisa");
+const professores = document.getElementById("professores");
+var searchtimer;
 
 const pesquisa = async (text) =>{
-    fetch("./includes/pesquisa.php", {
-        method: "POST",
-        body: text,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+    console.log(text)
+    try {
+        const response = await fetch("./includes/pesquisa.php", {
+            method: "POST",
+            body: text,
+            headers: {
+                'Content-Type': 'application/json' // Usando 'application/json' para o cabeçalho
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json(); // Aguarde a resolução da promessa JSON
+            professores.innerHTML = "";
+            if(data.erro){
+                professores.innerHTML = "<h1>Nenhum professor encontrado</h1>";
+            }else{
+                data.forEach(element => {
+                    professores.innerHTML += `
+                    <li class="professor">
+                    <img src="../fotosPerfil/${element.foto_professor}" alt="Foto do professor" class="imgProfessor">
+                    <h1 id="nomeProfessor">${element.nome_professor}</h1>
+                    <a id="saibaMais" href="saibamais.php?id=${element.id_professor}">Saiba Mais</a>
+                    </li>
+                    `;
+                });
+            }
+
+        } else {
+            console.error('Erro na solicitação:', response.status, response.statusText);
         }
-    }).then((response) => console.log(response.text()))
+    } catch (error) {
+        console.error('Erro ao processar a solicitação:', error);
+    }
 }
 
-text.addEventListener("keypress", (e) => {
-    var input = document.getElementById("inputPesquisa").value
-    const text = input+e.key
-    pesquisa(text)
-})
+
+window.addEventListener("DOMContentLoaded", () => {
+  text.addEventListener("input", (e) => {
+    clearTimeout(searchtimer);
+    searchtimer = setTimeout(() => {
+        const text = e.target.value
+        pesquisa(text)
+    }, 700);
+  });
+});
