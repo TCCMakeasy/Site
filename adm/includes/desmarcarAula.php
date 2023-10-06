4af50e75d756b4324a432d9f5ed15b2fc4fcd18b
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-10-06 16:20:26 -0300

    cronograma bugs

--- adm/includes/desmarcarAula.php
+++ adm/includes/desmarcarAula.php
@@ -9,10 +9,34 @@ if (!isset($_SESSION['id']) || $_SESSION['tipo'] != 3) {
     $data = explode(':', $data);
     $dia = $data[0];
     $horario = $data[1];
+    switch($dia){
+        case "seg":
+            $aulaDia = "Segunda-Feira";
+            break;
+        case "ter":
+            $aulaDia = "Terça-Feira";
+            break;
+        case "qua":
+            $aulaDia = "Quarta-Feira";
+            break;
+        case "qui":
+            $aulaDia = "Quinta-Feira";
+            break;
+        case "sex":
+            $aulaDia = "Sexta-Feira";
+            break;
+        case "sab":
+            $aulaDia = "Sábado";
+            break;
+        case "dom":
+            $aulaDia = "Domingo";
+            break;
+    }
     $request = "UPDATE cronograma SET `" . $dia . "_cronograma` = NULL WHERE id_professor = '" . $_SESSION['id'] . "' AND tempo_cronograma = '" . $horario . ":00:00'";
     if (mysqli_query($sql, $request)) {
         echo "Aula desmarcada com sucesso!";
     } else {
         echo "Erro ao desmarcar aula, tente novamente mais tarde!";
     }
+
 }
