d1181ba7249e65fc7380e1decb8908d2342ab8e0
unknown <tccmakeasy@gmail.com>
2023-10-26 11:05:18 -0300

    pw

--- aluno/includes/excluirConta.php
+++ aluno/includes/excluirConta.php
@@ -22,10 +22,39 @@ if (!isset($_SESSION['id'])) {
     else{
         echo 404;
     }
+
     $delete = "DELETE FROM aluno WHERE id_aluno = '$id'";
     $result = mysqli_query($sql, $delete)  or die (mysqli_error($sql));
+
     if($result){
         //Conta excluída com sucesso
+
+        $verifica = "SELECT * from aluno where id_aluno = ".$_SESSION['id']." AND id_professor is not NULL";
+        $verificaFinal = mysqli_query($sql, $verifica);
+
+        if(mysqli_num_rows($verificaFinal) > 0){
+
+        $data = data('m');
+        
+        $select = "SELECT * from armazena where mensal_armazena = $data";
+        $selectComando = mysqli_query($sql, $select);
+
+        $select1 = "SELECT * from armazena where id_professor = ".$_SESSION['id_professor']."";
+        $selectComando1 = mysqli_query($sql, $select1);	
+        
+        if(mysqli_num_rows($selectComando1) > 0 && (mysqli_num_rows($selectComando) > 0)){
+
+            $altera = "update armazena set perdidos_armazena = (perdidos_armazena + 1) where id_professor = ".$_SESSION['id_professor']." AND mensal_armazena = $data";
+            $alteraFinal = mysqli_query($sql, $altera);
+
+        }else{
+
+        $armazena = "INSERT into armazena(id_professor, perdidos_armazena, mensal_armazena) values ( ".$_SESSION['id_professor']." ,perdidos_armazena + 1, $data)";
+        $armazenaFinal = mysqli_query($sql, $armazena);
+
+        }
+    }
+
         session_destroy();
         header("Location: ../login.php");
     }else{
--- professor/includes/desvincular.php
+++ professor/includes/desvincular.php
@@ -12,6 +12,27 @@ if (!isset($_SESSION['id']) || $_SESSION['tipo'] != 2) {
     if ($resultDesvincularProfessor) {
         $_SESSION['msg'] = "Aluno desvinculado com sucesso!";
         header("Location: ../alunos.php");
+        
+        $data = date('m');
+
+        $select = "SELECT * from armazena where mensal_armazena = $data";
+        $selectComando = mysqli_query($sql, $select);
+
+        $select1 = "SELECT * from armazena where id_professor = ".$_SESSION['id']."";
+        $selectComando1 = mysqli_query($sql, $select1);	
+        
+        if(mysqli_num_rows($selectComando1) > 0 && (mysqli_num_rows($selectComando) > 0)){
+
+            $altera = "update armazena set perdidos_armazena = (perdidos_armazena + 1) where id_professor = ".$_SESSION['id']." AND mensal_armazena = $data";
+            $alteraFinal = mysqli_query($sql, $altera);
+
+        }else{
+
+        $armazena = "INSERT into armazena(id_professor, perdidos_armazena, mensal_armazena) values ( ".$_SESSION['id']." ,perdidos_armazena + 1, $data)";
+        $armazenaFinal = mysqli_query($sql, $armazena);
+
+        }
+
     } else {
         $_SESSION['msg'] = "Erro ao desvincular aluno";
         header("Location: ../alunos.php");
