4fa73dcd790a81000cb22311f4deb81eb7245bdd
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-10-27 21:25:47 -0300

    email

--- aluno/includes/Recuperar_Senha.php
+++ aluno/includes/Recuperar_Senha.php
@@ -1,9 +1,2 @@
 <?php
 
-    $email = 'davisousap1223@gmail.com';
-
-    mail($email, 'Recuperar senha',
-    '<html> <h1>Toque no botão abaixo para mudar sua senha</h1>
-    <a href="nova_senha.php">Mudar senha</a>"');
-
-?>
