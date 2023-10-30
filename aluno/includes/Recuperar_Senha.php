16d05cca18bce495fde172276c6ad275f3bd2d4b
unknown <tccmakeasy@gmail.com>
2023-10-30 10:14:48 -0300

    ipssi

--- aluno/includes/Recuperar_Senha.php
+++ aluno/includes/Recuperar_Senha.php
@@ -1,2 +1,52 @@
 <?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
 
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login - Aluno</title>
+  <link rel="stylesheet" type="text/css" href="../styles/estiloLoginCadastro.css" />
+  <link rel="shortcut icon" src="../images/favicon.ico" type="image/x-icon" />
+</head>
+
+<body>
+  <header>
+    <a>
+      <img id="voltar" src="../images/voltarseta.png" alt="Seta para voltar" />
+    </a>
+    <img src="../images/logo.png" alt="Logo da empresa" id="logo" />
+    <a id="span"></a>
+  </header>
+  <main>
+    <form method="post" id="cadastro" action="updateSenha.php?email=<?php echo $_GET['email'];?>&token=<?php echo $_GET['token'];?>">
+      <fieldset id="fieldsetLogin">
+        <h1 id="tituloLogin">Recuperar Senha</h1>
+
+        <label for="senha" class="tituloInputLogin">Digite sua nova senha:</label>
+        <input type="password" name="senha" id="senha" class="inputLogin" placeholder="●●●●●●●●" required />
+
+        <label for="senha" class="tituloInputLogin">Digite novamente:</label>
+        <input type="password" name="novaSenha" id="senha" class="inputLogin" placeholder="●●●●●●●●" required />
+        <div id="alert" class="avisos"><?php if (isset($_SESSION['msg'])) {echo $_SESSION['msg'];} ?></div>
+
+        <input type="submit" value="Mudar Senha" id="botaoSubmit" name="SenhaN"/>
+
+      </fieldset>
+    </form>
+  </main>
+</body>
+<script>
+  var alert = document.getElementById("alert");
+  if (alert.innerHTML != "") {
+    alert.style.display = "block";
+  }
+</script>
+<?php
+unset($_SESSION['msg']);
+?>
+
+</html>
---
+++ aluno/includes/updateSenha.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once '../../conexao.php';
+
+    $email = $_GET['email'];
+    $token = $_GET['token'];
+    $senha = $_POST['senha'];
+
+    $novaSenha = $_POST['novaSenha'];
+
+    if($senha == $novaSenha){
+
+    $novaSenha = password_hash($novaSenha, PASSWORD_DEFAULT);
+    $update = "UPDATE aluno set senha_aluno = '$novaSenha' WHERE email_aluno = '$email'";
+    $updateSQL = mysqli_query($sql, $update);
+    
+    
+    }else{
+
+        echo "Senha não é igual";
+
+    }
+
+
+?>
